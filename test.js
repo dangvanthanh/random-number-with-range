77b12562519cfb7b2fda48d4a5293eafc8c46d2b
Dang Van Thanh <dangvanthanh@dangthanh.org>
2016-06-15 21:36:56 +0700

    Use standard.js instead of xo

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-import test from 'ava';
-import fn from './';
+import test from 'ava'
+import fn from './'
 
 test('Random number from 1 to 50 with return float value', t => {
-	const rand = fn(1, 50);
+  const rand = fn(1, 50)
 
-	t.true(rand > 1 || rand < 50);
-});
+  t.true(rand > 1 || rand < 50)
+})
 
 test('Random number from 20 to 80 with return integer value', t => {
-	const rand = fn(20, 80, true);
+  const rand = fn(20, 80, true)
 
-	t.true((rand > 20 || rand < 80) && Math.round(rand) === rand);
-});
+  t.true((rand > 20 || rand < 80) && Math.round(rand) === rand)
+})
